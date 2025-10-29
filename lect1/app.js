4270e2fb9045ddf6a28d8b9d67b5cd5d649698f7
swarajdeshmukh <swarajd2002@gmail.com>
2025-10-30 00:51:53 +0530

    Import and export & dom basic

---
+++ lect1/app.js
@@ -0,0 +1,10 @@
+//There are two types of export default and name export
+
+// default export only single thing can be export
+const a = 10;
+export default a; 
+
+//name export 
+export const arr = [10, 20, 30, 40];
+export const user = 'Swaraj';
+
---
+++ lect1/script.js
@@ -0,0 +1,13 @@
+import a from './app.js' // default export name can be any thing
+import { arr, user } from './app.js' // name export 
+
+
+console.log(a);
+console.log(arr);
+console.log(user);
+
+//dom manipulation basic
+// var h1 = document.createElement("h1");
+// h1.innerHTML = "Hello";
+// // console.log(h1);
+// document.body.appendChild(h1);
