7b8504bcaa753be604c71176069100e29e2466d4
swarajdeshmukh <swarajd2002@gmail.com>
2025-11-06 16:07:13 +0530

    Project8 Learning completed function topic was cover

--- Project8 function/src/App.jsx
+++ Project8 function/src/App.jsx
@@ -1,34 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  
+  const [count, setCount]= useState(0);
+
+  // const counter = () =>{
+  //   setCount(count + 1);
+  //   if(count >= 10){
+  //     alert("Count limit reached");
+  //     setCount(0);
+  //   }
+  // }
+
+  // const userInput = (val) =>{
+  //   console.log(val.target.value)
+  // }
+  
+  // const userInput = (val) =>{
+  //   console.log(val)
+  // }
+
+  const mouseSpeed = (val) =>{
+    if(val > 0){
+      console.log("niche jaa raha hai")
+    }else{
+      console.log("uppar jaa raha hai")
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='parent' onWheel={(elem) =>{
+      mouseSpeed(elem.deltaY)
+    }} >
+      {/* <div className="child">
+        <h1>{count}</h1>
+        <button onClick={counter}>Click me</button>
+
+          <input onChange={userInput} type="text" placeholder='Enter your name...' />
+
+          <input onChange={function(elem){
+            userInput(elem.target.value)
+          }} type="text" placeholder='Enter your name...' />
+          
+      </div> */}
+
+      <div id="page1"></div>
+      <div id="page2"></div>
+      <div id="page3"></div>
+      
+    </div>
   )
 }
 
