cb06de5d530613bb2488ad9a7d9d82d812026f16
swarajdeshmukh <swarajd2002@gmail.com>
2025-11-08 16:05:44 +0530

    Update to project 12

--- project12/src/App.jsx
+++ project12/src/App.jsx
@@ -1,8 +1,40 @@
-import { useEffect, useState } from 'react'
-
+// import axios from 'axios'
+import { useState, useEffect } from 'react'
 
 
 const App = () => {
+
+//  async function getData(){
+//     const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+//     console.log(response)
+//   }
+
+  // const getData = async () => {
+  //   const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+  //   const data = await response.json()
+
+  //   console.log(data)
+
+  //   // console.log(response)
+  // }
+
+  // API calling using  axios
+
+
+  // const getData = async () =>{
+  //   const {data} = await axios.get('https://jsonplaceholder.typicode.com/users')
+
+  //   console.log(data)
+  // }
+
+  // const getData = async () => {
+  //   let {data} = await axios.get('https://picsum.photos/v2/list')
+  //   console.log(data)
+  // }
+
+  // Learning useEffect hook
 
   const [num1, setNum1] = useState(0)
   const [num2, setNum2] = useState(100)
@@ -20,11 +52,17 @@ const App = () => {
   }
 
   return (
-    <div>
-      <h1>number 1 = {num1}</h1>
-      <h1>number 2 = {num2} </h1>
-      <button onMouseEnter={mEnter} onMouseLeave={mLeave}>Hover Me</button>
-    </div>
+    // <div>
+    //   <button onClick={getData}>Get Data</button>
+    // </div>
+
+
+      <div>
+        <h1>number 1 = {num1}</h1>
+        <h1>number 2 = {num2} </h1>
+        <button onMouseEnter={mEnter} onMouseLeave={mLeave}>Hover Me</button>
+      </div>
+     
   )
 }
 
